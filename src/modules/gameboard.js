438f4083b44bab7a3cd7e53ad74c69d716446338
Adam Forgac <aforg@email.cz>
2023-10-31 15:22:49 +0100

    Create function which marks dead ships
    This function add sank class to dead ships

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,22 +1,45 @@
-import Ship from './ship';
-
+/* eslint-disable class-methods-use-this */
 export default class Gameboard {
   constructor() {
     this.occupiedCoordinates = [];
     this.bombedCoordinates = [];
-    this.emptyCoordinates = [];
     this.ships = [];
+  }
+
+  updateBombed(bombedItem) {
+    this.bombedCoordinates.push(bombedItem);
   }
 
   updateCoordinates(occupiedItem) {
     this.occupiedCoordinates.push(occupiedItem);
   }
 
+  markSunkShips(ship, field) {
+    const classes = [];
+    const playField = document.querySelector(`.${field}`);
+    const startPosition = ship.position;
+    const givenLength = ship.length;
+    const [prefix, row, col] = startPosition.split('-');
+    if (ship.orientation === 'AXIS: X') {
+      for (let i = 0; i < givenLength; i++) {
+        classes.push(`${prefix}-${row}-${Number(col) + i}`);
+      }
+    } else {
+      for (let i = 0; i < givenLength; i++) {
+        classes.push(`${prefix}-${Number(row) + i}-${col}`);
+      }
+    }
+
+    classes.forEach((cellString) => {
+      const element = playField.querySelector(`.${cellString}`);
+      element.classList.add('sank');
+    });
+  }
+
   checkOccupied(row, col, orientation, length) {
     if (orientation === 'AXIS: X') {
       for (let i = 0; i < length; i++) {
         const spot = `cell-${row}-${col + i}`;
-        console.log(spot);
         if (this.occupiedCoordinates.includes(spot)) {
           return true;
         }
@@ -24,17 +47,16 @@ export default class Gameboard {
     } else {
       for (let i = 0; i < length; i++) {
         const spot = `cell-${row + i}-${col}`;
-        console.log(spot);
         if (this.occupiedCoordinates.includes(spot)) {
           return true;
         }
       }
     }
 
-    console.log(this.occupiedCoordinates);
-
     return false;
   }
+
+  // CREATES THE GAME GRID IN DOM
 
   showBoard(className) {
     const gridContainer = document.querySelector(`.${className}`);
@@ -51,49 +73,48 @@ export default class Gameboard {
     }
   }
 
-  showShips() {
-    console.log(this.ships);
-    console.log(this.occupiedCoordinates);
-  }
-
   showCurrentSize() {
     const infoArr = [this.ships[this.ships.length - 1], this.ships[this.ships.length - 2]];
     return infoArr;
   }
 
-  placeImage(ship, gameField) {
-    const cellSize = 48;
+  placeImage(ship, gameField, status, oldImage) {
+    let gap = 4;
+    if (window.innerWidth < 1070) gap = 2;
+    const gridCell = document.querySelector('.grid-cell');
+    const cellSize = gridCell.offsetWidth;
     const positionParts = ship.position.split('-');
     const row = Number(positionParts[1]);
     const col = Number(positionParts[2]);
     let leftMargin;
     let topMargin;
-    const finalSize = cellSize * ship.length;
+    const finalSize = ((cellSize + gap) * ship.length);
     const field = document.querySelector(`.${gameField}`);
     const startingPoint = field.querySelector(`.${ship.position}`);
-    console.log(startingPoint);
+    const imageToRemove = startingPoint.querySelector('img');
     const shipImage = document.createElement('img');
     if (ship.orientation === 'AXIS: X') {
       shipImage.classList.add('ship-image-x');
-      topMargin = 50 * (row - 1);
-      leftMargin = 50 * (col - 1);
+      topMargin = (gridCell.offsetWidth + gap) * (row - 1);
+      leftMargin = (gridCell.offsetWidth + gap) * (col - 1);
     } else {
       shipImage.classList.add('ship-image-y');
-      topMargin = 50 * (row - 1);
-      leftMargin = 50 * (col - 1);
+      topMargin = (gridCell.offsetWidth + gap) * (row - 1);
+      leftMargin = (gridCell.offsetWidth + gap) * (col - 1);
     }
     shipImage.style.width = `${finalSize}px`;
     shipImage.style.top = `${topMargin}px`;
     shipImage.style.left = `${leftMargin}px`;
-    shipImage.src = `images/${ship.length}-ship.png`;
+    if (oldImage) {
+      imageToRemove.remove();
+    }
+    if (status) {
+      if (ship.length === 2) shipImage.classList.add(`length-${2}`);
+      shipImage.src = `images/${ship.length}-ship-dead.png`;
+    } else {
+      if (ship.length === 2) shipImage.classList.add(`length-${2}`);
+      shipImage.src = `images/${ship.length}-ship.png`;
+    }
     startingPoint.appendChild(shipImage);
-  }
-
-  receiveAttack(row, col) {
-
-  }
-
-  sunkAllShips() {
-
   }
 }
