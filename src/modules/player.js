c3795dd9166422ad47563ff090fa63471df2f499
Adam Forgac <aforg@email.cz>
2023-10-31 15:23:50 +0100

    Let the system recognize what ship has been hit

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,5 +1,7 @@
+import showWinner from './determineWinner';
+
 const player = {
-  playPlayer(event, playerBoard, botBoard) {
+  playerAttack(event, playerBoard, botBoard) {
     const domBotBoard = document.querySelector('.bot-field');
     const clickedElement = event.target;
     const classes = clickedElement.classList;
@@ -8,7 +10,25 @@ const player = {
       const chosenSpot = classesArray[1];
       if (botBoard.occupiedCoordinates.includes(chosenSpot)) {
         const spotIndex = botBoard.occupiedCoordinates.indexOf(chosenSpot);
-        botBoard.occupiedCoordinates.splice(spotIndex, 1);
+        let shipHit;
+
+        if (spotIndex >= 0 && spotIndex < 5) {
+          shipHit = botBoard.ships[0];
+        } else if (spotIndex < 9) {
+          shipHit = botBoard.ships[1];
+        } else if (spotIndex < 12) {
+          shipHit = botBoard.ships[2];
+        } else if (spotIndex < 15) {
+          shipHit = botBoard.ships[3];
+        } else {
+          shipHit = botBoard.ships[4];
+        }
+
+        shipHit.hit();
+
+        if (shipHit.isSunk()) {
+          botBoard.placeImage(shipHit, 'bot-field', true);
+        }
 
         const domChosenSpot = domBotBoard.querySelector(`.${chosenSpot}`);
         const hit = document.createElement('div');
@@ -21,11 +41,14 @@ const player = {
         domChosenSpot.appendChild(noHit);
       }
       botBoard.bombedCoordinates.push(chosenSpot);
+
+      setTimeout(showWinner, 1000, botBoard, playerBoard);
+
       this.playStatus = false;
+
       return true;
-    } else {
-      return false;
     }
+    return false;
   },
 
   playStatus: true,
