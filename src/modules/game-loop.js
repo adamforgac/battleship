dd4f8f94e8ba1ef3dfb0e15f4dd32844f4b53c3a
Adam Forgac <aforg@email.cz>
2023-10-31 15:24:26 +0100

    Finish the game loop

--- src/modules/game-loop.js
+++ src/modules/game-loop.js
@@ -1,16 +1,24 @@
 import computer from './computer';
 import player from './player';
+import { callWinner } from './dom';
+
+// MANAGES THE FLOW OF THE GAME
 
 export default function gameLoop(playerBoard, botBoard) {
   const domBotBoard = document.querySelector('.bot-field');
 
   domBotBoard.addEventListener('click', (e) => {
     if (player.playStatus) {
-      const round = player.playPlayer(e, playerBoard, botBoard);
+      const round = player.playerAttack(e, playerBoard, botBoard);
 
       if (round) {
         setTimeout(() => {
-          computer.playComputer(playerBoard.occupiedCoordinates, playerBoard.bombedCoordinates, playerBoard);
+          computer.playComputer(
+            playerBoard.occupiedCoordinates,
+            playerBoard.bombedCoordinates,
+            playerBoard,
+            botBoard,
+          );
         }, 1000);
       }
     }
